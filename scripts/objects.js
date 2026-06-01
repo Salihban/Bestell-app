f5aae926be4f1055b77a06a47ff001cb1c9d45a1
Salih Derin <derin.salih@gmx.de>
2026-06-01 19:02:08 +0200

    obj pizza and salad

--- scripts/objects.js
+++ scripts/objects.js
@@ -3,7 +3,7 @@ let burgerMenu = [
     title:"Veggie mushroom black burger",
     price: 16,99€,
     description: "Mixed green salad, Tomatoes, Edamame, Mushrooms",
-    image: "img/veggie-burger - Kopie.jpg"
+    image: "img/veggie-burger.jpg"
 },
     {
     title:"All meat burger",
@@ -26,16 +26,56 @@ title:"Beef red burger",
 ];
 
 
-let pizzaMenu = {
-    title:"",
-    price: ,
-    description: "",
-    image: ""
+let pizzaMenu = [
+    {
+    title:"Pizza Margherita",
+    price: 11,90€,
+    description: "Tomato Sauce, Mozzarella",
+    image: "img/pizza-margherita.jpg"
+},
+    {
+    title:"Pizza Chorizo",
+    price: 13,90€,
+    description: "Tomato slices, Mozzarella, Chorizo",
+    image: "img/Pizza-chorizo.jpg"
+},
+{
+title:"Funghi",
+    price: 12,90€,
+    description: "Red onion, Olives, Button Mushrooms, Mozzarella",
+    image: "img/pizza-funghi.jpg"
+},
+{
+    title:"Quattro Formaggi with Chicken ",
+    price: 15,90€,
+    description: "Chicken, Mozzarella, Gorgonzola, Fontina,  Parmigiano Reggiano",
+    image: "img/pizza-quattro.jpg"
 }
+];
 
-let saladMenu = {
-    title:"",
-    price: ,
-    description: "",
-    image: ""
+let saladMenu = [
+    {
+    title:"Warm beef arugula salad",
+    price: 16,90€,
+    description: "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
+    image: "img/beef-salad.jpg"
+},
+{
+    title:"Mini green Salad",
+    price: 7,90€,
+    description: "Green salad, Cucumber, Carrots, Parsley, Radishes",
+    image: "img/mini-salad.jpg"
+},
+{
+    title:"Green Salad with sea food",
+    price: 16,90€,
+    description: "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+    image: "img/green-salad.jpg"
+},
+{
+    title:"Vegan green salad with tofu",
+    price: 14,90€,
+    description: "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+    image: "img/vegan-salad.jpg"
 }
+];
