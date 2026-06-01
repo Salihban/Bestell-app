c5b69f3ebb91afce43e467633337756ce7ad3519
Salih Derin <derin.salih@gmx.de>
2026-06-01 18:21:30 +0200

    objekt basic

---
+++ scripts/objects.js
@@ -0,0 +1,20 @@
+let burgerMenu = {
+    title:"",
+    price: ,
+    description: "",
+    image: ""
+}
+
+let pizzaMenu = {
+    title:"",
+    price: ,
+    description: "",
+    image: ""
+}
+
+let saladMenu = {
+    title:"",
+    price: ,
+    description: "",
+    image: ""
+}
