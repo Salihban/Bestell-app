e01c3c5e998632d9d0d80d9e93f51fa5390b4a4f
Salih Derin <derin.salih@gmx.de>
2026-06-01 18:35:11 +0200

    obj burger

--- scripts/objects.js
+++ scripts/objects.js
@@ -1,9 +1,30 @@
-let burgerMenu = {
-    title:"",
-    price: ,
-    description: "",
-    image: ""
+let burgerMenu = [
+    {
+    title:"Veggie mushroom black burger",
+    price: 16,99€,
+    description: "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+    image: "img/veggie-burger - Kopie.jpg"
+},
+    {
+    title:"All meat burger",
+    price: 15,90€,
+    description: "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
+    image: "img/all-meat-burger.jpg"
+},
+{
+title:"Beef red burger",
+    price: 14,90€,
+    description: "Beef, Cheese, Tomatoes, Lettuce, Onion",
+    image: "img/beef-red-burger.jpg"
+},
+{
+    title:"Big chicken burger",
+    price: 15,90€,
+    description: "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
+    image: "img/big-chicken-burger.jpg"
 }
+];
+
 
 let pizzaMenu = {
     title:"",
