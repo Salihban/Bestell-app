662a3ae6bec9fba05472cc891fbab35ccfdc4e2d
Salih Derin <derin.salih@gmx.de>
2026-06-02 11:54:51 +0200

    obj id

--- scripts/objects.js
+++ scripts/objects.js
@@ -1,24 +1,28 @@
 let burgerMenu = [
     {
-    title:"Veggie mushroom black burger",
+    id:"burger_01"
+    name:"Veggie mushroom black burger",
     price: 16.99,
     description: "Mixed green salad, Tomatoes, Edamame, Mushrooms",
     image: "img/veggie-burger.jpg"
 },
     {
-    title:"All meat burger",
+    id:"burger_02"
+    name:"All meat burger",
     price: 15.90,
     description: "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
     image: "img/all-meat-burger.jpg"
 },
 {
-title:"Beef red burger",
+    id:"burger_03"
+    name:"Beef red burger",
     price: 14.90,
     description: "Beef, Cheese, Tomatoes, Lettuce, Onion",
     image: "img/beef-red-burger.jpg"
 },
 {
-    title:"Big chicken burger",
+    id:"burger_04"
+    name:"Big chicken burger",
     price: 15.90,
     description: "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
     image: "img/big-chicken-burger.jpg"
@@ -27,26 +31,30 @@ title:"Beef red burger",
 
 
 let pizzaMenu = [
-    {
-    title:"Pizza Margherita",
+{
+    id:"pizza_01"
+    name:"Pizza Margherita",
     price: 11.90,
     description: "Tomato Sauce, Mozzarella",
     image: "img/pizza-margherita.jpg"
 },
     {
-    title:"Pizza Chorizo",
+    id:"pizza_02"
+    name:"Pizza Chorizo",
     price: 13.90,
     description: "Tomato slices, Mozzarella, Chorizo",
     image: "img/Pizza-chorizo.jpg"
 },
 {
-title:"Funghi",
+    id:"pizza_03"
+    name:"Funghi",
     price: 12.90,
     description: "Red onion, Olives, Button Mushrooms, Mozzarella",
     image: "img/pizza-funghi.jpg"
 },
 {
-    title:"Quattro Formaggi with Chicken ",
+    id:"pizza_04"
+    name:"Quattro Formaggi with Chicken ",
     price: 15.90,
     description: "Chicken, Mozzarella, Gorgonzola, Fontina,  Parmigiano Reggiano",
     image: "img/pizza-quattro.jpg"
@@ -54,26 +62,30 @@ title:"Funghi",
 ];
 
 let saladMenu = [
-    {
-    title:"Warm beef arugula salad",
+{
+    id:"salad_01"
+    name:"Warm beef arugula salad",
     price: 16.90,
     description: "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
     image: "img/beef-salad.jpg"
 },
 {
-    title:"Mini green Salad",
+    id:"salad_02"
+    name:"Mini green Salad",
     price: 7.90,
     description: "Green salad, Cucumber, Carrots, Parsley, Radishes",
     image: "img/mini-salad.jpg"
 },
 {
-    title:"Green Salad with sea food",
+    id:"salad_03"
+    name:"Green Salad with sea food",
     price: 16.90,
     description: "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
     image: "img/green-salad.jpg"
 },
 {
-    title:"Vegan green salad with tofu",
+    id:"salad_04"
+    name:"Vegan green salad with tofu",
     price: 14.90,
     description: "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
     image: "img/vegan-salad.jpg"
--- scripts/template.js
+++ scripts/template.js
@@ -1,5 +1,9 @@
 function getBurgerTemplate(i) {
     return /*html*/`
-    
+    <div class="content">
+        <div>
+            <img src="./assets/img/${burgerMenu[i]}" alt="">
+        </div>
+    </div>
     `
 }
