620b7accc80c12bab7681ecc78dd22161dd25bbb
Salih Derin <derin.salih@gmx.de>
2026-06-02 11:57:04 +0200

    ,

--- scripts/objects.js
+++ scripts/objects.js
@@ -1,27 +1,27 @@
 let burgerMenu = [
     {
-    id:"burger_01"
+    id:"burger_01",
     name:"Veggie mushroom black burger",
     price: 16.99,
     description: "Mixed green salad, Tomatoes, Edamame, Mushrooms",
     image: "img/veggie-burger.jpg"
 },
     {
-    id:"burger_02"
+    id:"burger_02",
     name:"All meat burger",
     price: 15.90,
     description: "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
     image: "img/all-meat-burger.jpg"
 },
 {
-    id:"burger_03"
+    id:"burger_03",
     name:"Beef red burger",
     price: 14.90,
     description: "Beef, Cheese, Tomatoes, Lettuce, Onion",
     image: "img/beef-red-burger.jpg"
 },
 {
-    id:"burger_04"
+    id:"burger_04",
     name:"Big chicken burger",
     price: 15.90,
     description: "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
@@ -32,28 +32,28 @@ let burgerMenu = [
 
 let pizzaMenu = [
 {
-    id:"pizza_01"
+    id:"pizza_01",
     name:"Pizza Margherita",
     price: 11.90,
     description: "Tomato Sauce, Mozzarella",
     image: "img/pizza-margherita.jpg"
 },
     {
-    id:"pizza_02"
+    id:"pizza_02",
     name:"Pizza Chorizo",
     price: 13.90,
     description: "Tomato slices, Mozzarella, Chorizo",
     image: "img/Pizza-chorizo.jpg"
 },
 {
-    id:"pizza_03"
+    id:"pizza_03",
     name:"Funghi",
     price: 12.90,
     description: "Red onion, Olives, Button Mushrooms, Mozzarella",
     image: "img/pizza-funghi.jpg"
 },
 {
-    id:"pizza_04"
+    id:"pizza_04",
     name:"Quattro Formaggi with Chicken ",
     price: 15.90,
     description: "Chicken, Mozzarella, Gorgonzola, Fontina,  Parmigiano Reggiano",
@@ -63,28 +63,28 @@ let pizzaMenu = [
 
 let saladMenu = [
 {
-    id:"salad_01"
+    id:"salad_01",
     name:"Warm beef arugula salad",
     price: 16.90,
     description: "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
     image: "img/beef-salad.jpg"
 },
 {
-    id:"salad_02"
+    id:"salad_02",
     name:"Mini green Salad",
     price: 7.90,
     description: "Green salad, Cucumber, Carrots, Parsley, Radishes",
     image: "img/mini-salad.jpg"
 },
 {
-    id:"salad_03"
+    id:"salad_03",
     name:"Green Salad with sea food",
     price: 16.90,
     description: "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
     image: "img/green-salad.jpg"
 },
 {
-    id:"salad_04"
+    id:"salad_04",
     name:"Vegan green salad with tofu",
     price: 14.90,
     description: "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
