305ad3e8430171ae2d1703fb732ffafc8e49c056
Salih Derin <derin.salih@gmx.de>
2026-06-02 12:56:38 +0200

    icons

--- scripts/template.js
+++ scripts/template.js
@@ -2,7 +2,11 @@ function getBurgerTemplate(i) {
     return /*html*/`
     <div class="content">
         <div>
-            <img src="./assets/img/${burgerMenu[i]}" alt="">
+            <img src="./assets/img/${burgerMenu[i]}.id" alt="">
+        </div>
+
+        <div>
+            <h3>${burgerMenu[i].name}</h3>
         </div>
     </div>
     `
