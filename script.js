8febfd23351f763e0ddd58a262c6aaf7a58a60d6
Salih Derin <derin.salih@gmx.de>
2026-06-04 10:44:51 +0200

    render menu

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function init(){
     renderBurger();
+    renderPizza();
+    renderSalad();
 }
 
 
@@ -9,5 +11,23 @@ function renderBurger() {
 
     for (let i = 0; i < burgerMenu.length; i++) {
         contentRef.innerHTML += getBurgerTemplate(burgerMenu[i]);
+    }
+}
+
+function renderPizza() {
+    let contentRef = document.getElementById("pizzaCard");
+    contentRef.innerHTML = "";
+
+    for (let i = 0; i < burgerMenu.length; i++) {
+        contentRef.innerHTML += getPizzaTemplate(pizzaMenu[i]);
+    }
+}
+
+function renderSalad() {
+    let contentRef = document.getElementById("saladCard");
+    contentRef.innerHTML = "";
+
+    for (let i = 0; i < burgerMenu.length; i++) {
+        contentRef.innerHTML += getSaladTemplate(saladMenu[i]);
     }
 }
--- scripts/template.js
+++ scripts/template.js
@@ -9,3 +9,27 @@ function getBurgerTemplate(burger) {
     </div>
     `;
 }
+
+function getPizzaTemplate(pizza) {
+    return /*html*/`
+    <div class="menu-card">
+        <img src="./assets/${pizza.image}" alt="${pizza.name}">
+        <h3>${pizza.name}</h3>
+        <p>${pizza.description}</p>
+        <span>${pizza.price}€</span>
+        <button>Add to basket</button>
+    </div>
+    `;
+}
+
+function getSaladTemplate(salad) {
+    return /*html*/`
+    <div class="menu-card">
+        <img src="./assets/${salad.image}" alt="${salad.name}">
+        <h3>${salad.name}</h3>
+        <p>${salad.description}</p>
+        <span>${salad.price}€</span>
+        <button>Add to basket</button>
+    </div>
+    `;
+}
