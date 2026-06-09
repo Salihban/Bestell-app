66a85cc04366fb6a06a3e918b466d89654a57a57
Salih Derin <derin.salih@gmx.de>
2026-06-09 15:32:05 +0200

    render basket/addtobasket

--- script.js
+++ script.js
@@ -22,25 +22,40 @@ function renderSection(containerId, sectionIndex) {
 }
 
 function renderBasket() {
+    let basketRef = document.getElementById('basket-cart');
+    basketRef.innerHTML = "";
+
+    for (let i = 0; i < basket.length; i++) {
+        let item = basket[i];
+    }
     
-
-
-
+    basketRef.innerHTML += getBasketTemplate();
 }
 
-function addToBasket(basketEmpty) {
-    let foundItem = basket.find((items) => items.id === dishId);
+function addToBasket(dishId) {
+    let foundDish = null;
 
-    if (foundItem) {
-        foundItem.count++;
+    for (let k = 0; k < menuData.length; k++) {
+    
+    let category = menuData[k];
+    let item = category.items.find(items => items.id === dishId);
+    if (item){
+    foundDish = item;
+    }
+    }
+
+    let foundInBasket = basket.find(item => item.id === dishId);
+
+    if (foundInBasket) {
+        foundInBasket.count++;
     } else {
         basket.push({
-            id: dish.id,
-            name: dish.name,
-            price: dish.price,
+            id: foundDish.id,
+            name: foundDish.name,
+            price: foundDish.price,
             count: 1
         })
     }
-    
+
     renderBasket();
 }
